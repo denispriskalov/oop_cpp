18ff7d17432e40cc6ad7c8ab545dd968a40ce4f7
Denis Priskalov <denispriskalov@mail.ru>
2017-11-02 13:46:26 +0300

    Change programm

--- main.cpp
+++ main.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class Base
-{
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
-
-    void func() { cout << "Function func() of class Base\n"; }
-};
-
-class Child: public Base
-{
-public:
-    Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
-
-    void func() { cout << "Function func() of class Child\n"; }
-};
-
-int main(int argc, char *argv[])
-{
-    cout << "* Create Object base" << endl;
-    Base base;
-
-    cout << "* Create Object child" << endl;
-    Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    base.func();
-
-    cout << "* Call method func() for object child" << endl;
-    child.func();
+int main()
+{string str; unsigned int k=0, pos=0;
+cout<<"Enter string"<<endl;
+getline(cin,str);
+while (k<str.length())
+{ if (str[k]>='0' && str[k]<='9')
+str.erase(k,1);
+else k++;}
+cout<<"string="<<str<<endl;
 
     return 0;
 }
