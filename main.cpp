f394f665adfa267bdbcefc4a2ca114cd603c46de
Denis Priskalov <denispriskalov@mail.ru>
2017-11-02 13:35:24 +0300

    Change programm

--- main.cpp
+++ main.cpp
@@ -1,36 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
-class Base
-{
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
-
-    void func() { cout << "Function func() of class Base\n"; }
-};
-
-class Child: public Base
-{
-public:
-    Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
-};
-
-int main(int argc, char *argv[])
-{
-    cout << "* Create Object base" << endl;
-    Base base;
-
-    cout << "* Create Object child" << endl;
-    Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    base.func();
-
-    cout << "* Call method func() for object child" << endl;
-    child.func();
-
+ int main()
+{float a[100];
+int n; cout << "n="; cin>>n;
+float s=0;
+for (int i=0; i<n; ++i)
+{cout<<"a["<<i<<"]="; cin>>a[i];
+s+=a[i];}
+cout<<"s="<<s<<endl;
     return 0;
 }
