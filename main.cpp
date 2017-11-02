ac9a59d4bf35d29b3078062f9876afb269cbd015
Denis Priskalov <denispriskalov@mail.ru>
2017-11-02 14:10:33 +0300

    Change 4

--- main.cpp
+++ main.cpp
@@ -1,44 +1,18 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
-
-class Base
-{
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
-
-    void func() const { cout << "Function func() of class Base\n"; }
-};
-
-class Child: public Base
-{
-public:
-    Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
-
-    void func() const { cout << "Function func() of class Child\n"; }
-};
-
-void Function(const Base &obj)
-{
-    cout << "* Call method func() for object obj" << endl;
-    obj.func();
-}
-
-int main(int argc, char *argv[])
-{
-    cout << "* Create Object base" << endl;
-    Base base;
-
-    cout << "* Create Object child" << endl;
-    Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    Function(base);
-
-    cout << "* Call method func() for object child" << endl;
-    Function(child);
+int main()
+{char str[20];
+char symbol;
+int k=0;
+cout<<"Enter string"<<endl;
+cin.getline(str,20);
+cout<<"Enter symbol"<<endl;
+cin >> symbol;
+for( unsigned int i=0; i<strlen(str); ++i)
+if (str[i]==symbol) k++;
+cout<<"k="<<k<<endl;
 
     return 0;
 }
